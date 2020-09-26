6f026ce90d353ecfad48df166e26590e502e853a
Duy <10cb4tranbaoduy@gmail.com>
2020-09-26 16:12:07 +0700

    Signup validation

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const app = require('express')();
 const admin = require('firebase-admin')
 const functions = require('firebase-functions');
-const firebase = require('firebase')
+const firebase = require('firebase');
+const { exampleIssue } = require('firebase-functions-test/lib/providers/crashlytics');
 
 // admin setup
 admin.initializeApp();
@@ -64,6 +65,23 @@ app.post('/screams', (req, res) => {
       })
 })
 
+const idEmail = (email) => {
+   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+   if (email.match(regex)) {
+      return true;
+   } else {
+      return false;
+   }
+}
+
+const isEmpty = (string) => {
+   if (string.trim() === '') {
+      return true;
+   } else {
+      return false;
+   }
+}
+
 // Signup Route
 app.post('/signup', (req, res) => {
    const newUser = {
@@ -73,12 +91,28 @@ app.post('/signup', (req, res) => {
       handle: req.body.handle
    }
 
+   let errors = {};
+   if (isEmpty(newUser.email)) {
+      errors.email = 'Email must not be empty';
+   } else {
+      if (isEmpty(newUser.email)) {
+         errors.email = 'Email must be a email address';
+      }
+   }
+   if (newUser.password === "") errors.password = 'Must not be empty';
+   if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = "Password must match"
+   if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+   if (Object.keys(errors).length > 0) {
+      // 400 = bad request
+      res.status(400).json(errors);
+   }
+
    // TODO: vadidate Data
    // Doc(/users/${newUser.handle}) -> kiểm tra doc.exists -> chưa tồn tại -> 
    // Tạo user và lưu vào aithentication -> Trả về Promise  (data)  ->  // Trả về Promise (token) -> Lấy token là lưu vào collection: users của cloud store
    let token, userID;
    admin.firestore().doc(`/users/${newUser.handle}`).get()
-      .then(doc => { 
+      .then(doc => {
          if (doc.exists) {
             res.status(400).json({ handle: `this handle is already taken` })
          } else {
@@ -101,17 +135,17 @@ app.post('/signup', (req, res) => {
          }
          admin.firestore().doc(`/users/${newUser.handle}`).set(userCredential) // Trả về 1 Promise   
       })
-      .then(()=> {
-         return res.status(201).json({token})
+      .then(() => {
+         return res.status(201).json({ token })
       })
       .catch(err => { // Bắt lỗi khi post lên user đã tồn tại
          console.log(err)
          if (err.code === "auth/email-already-in-use") {
-            return res.status(500).json({email: `Email is already used`})
+            return res.status(500).json({ email: `Email is already used` })
          } else {
-            return res.status(500).json({err: err.code})
+            return res.status(500).json({ err: err.code })
          }
-      })  
+      })
 })
 
 //*  https://baseurl.com/api/
